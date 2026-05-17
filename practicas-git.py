d1aef17b866ae37f3a26900d96782ca4ec08cc69
john <jlima@fi.uba.ar>
2026-05-17 16:59:32 -0300

    tercer commit

--- practicas-git.py
+++ practicas-git.py
@@ -8,3 +8,8 @@ print("creando ramas")
 #git branch "nombre de la rama" para crear una rama
 #git push -u origin "nombre de la rama" para subir la rama a github
 #git switch "nombre de la rama" para cambiar a esa rama
+print("fusionando ramas")
+#git switch "rama a la que se va a fusionar" para cambiar a esa rama
+#git pull para actualizar la rama
+#git merge "rama que se va a fusionar" para fusionar la rama
+#git push para subir los cambios a github
