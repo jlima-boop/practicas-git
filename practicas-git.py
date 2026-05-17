936e6da5ccbb600368b79d0c7e6ca7808b53a699
john <jlima@fi.uba.ar>
2026-05-17 16:54:17 -0300

    segundo commit

--- practicas-git.py
+++ practicas-git.py
@@ -1,1 +1,10 @@
 print("practica de git")
+print("creando una repositorio")
+#git init "nombre del repositorio"
+#git remote add origin "url del repositorio" lo vincula con github
+#git branch -M main lo cambia a main
+#git push -u origin main  lo sube a github
+print("creando ramas")
+#git branch "nombre de la rama" para crear una rama
+#git push -u origin "nombre de la rama" para subir la rama a github
+#git switch "nombre de la rama" para cambiar a esa rama
